4faa2320c86ae13792c3ec540b7779b162e6e03b
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2025-05-11 01:23:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export default app;
