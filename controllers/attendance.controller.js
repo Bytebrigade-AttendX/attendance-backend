833e8dccbb41fc2792c5b57e969cb968ba8af6ef
Aadarsh Agarwal <140622088+aadarshagarwal1@users.noreply.github.com>
2025-06-09 19:35:49 +0530

    testing

--- controllers/attendance.controller.js
+++ controllers/attendance.controller.js
@@ -418,7 +418,7 @@ const getMarked = async (req, res) => {
       parseFloat(attendance[0].teacherLongitude),
       parseFloat(studentLat),
       parseFloat(studentLon),
-      20000
+      100000
     );
 
     console.log(attendance[0].teacherLatitude);
